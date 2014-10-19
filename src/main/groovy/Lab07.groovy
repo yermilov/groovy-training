f2af1968a13243aaaf331678ef36af714ce10034
yaroslav.yermilov <yermilov17@gmail.com>
2014-10-19 15:32:49 +0300

    lab07

--- src/main/groovy/Lab07.groovy
+++ src/main/groovy/Lab07.groovy
@@ -3,15 +3,22 @@
  */
 
 def group(List list, Closure equalator) {
-    def result = [:]
+    def result = []
 
     list.each { elem1 ->
-        result[elem1] = list.count { elem2 ->
-            equalator elem1, elem2
+        def dest = result.find { elem2 ->
+            equalator(elem1, elem2[0])
+        }
+        if (dest) {
+            dest << elem1
+        } else {
+            result << [ elem1 ]
         }
     }
 
     return result
 }
 
-assert group([1,2,3,4,5]) { o1, o2 -> o1 % 2 == o2 % 2 } == [ 0:2, 1:3 ]
+assert group([1,2,3,4,5]) { o1, o2 -> o1 % 2 == o2 % 2 } == [ [1, 3, 5], [2, 4] ]
+
+assert group(["a1","a2","b3","b4","c5"]) { o1, o2 -> o1[0] == o2[0] } == [ ["a1","a2"], ["b3","b4"], ["c5"] ]
